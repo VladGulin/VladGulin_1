33a24a234b4155d7bea7ed2a3465055927a2ccff
VladGulin <gvitaly1603@gmail.com>
2025-04-10 17:03:08 +0300

    this is a ping pong

--- Algoritmika1/main.py
+++ Algoritmika1/main.py
@@ -14,16 +14,20 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_right(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_w] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < 700 - 80:
-            self.rect.x += self.speed
+        if keys[K_s] and self.rect.y < height - 80:
+            self.rect.y += self.speed
+
+    def update_left(self):
+        keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 500 - 80:
+        if keys[K_DOWN] and self.rect.y < height - 80:
             self.rect.y += self.speed
+
 
 wight = 500
 height = 500
